3ca68671339afc7360417551541442ad9a508ad2
Brandi Abernathy <brandiabernathy22@gmail.com>
2023-07-02 21:21:26 -0700

    addressing some ts errors

--- context/app.tsx
+++ context/app.tsx
@@ -2,14 +2,22 @@
 
 import { createContext, useContext, useState } from "react";
 
-const AppContext = createContext({})
+const AppContext = createContext({
+    user: {
+        name: '',
+        id: ''
+    },
+    setUser: (user: any) => {},
+    week: 0,
+    setWeek: (week: any) => {},
+});
 
-export const AppContextProvider = ({ children }) => {
+export const AppContextProvider = ({ children }: any) => {
     const [user, setUser] = useState({});
     const [week, setWeek] = useState(0);
 
     return (
-        <AppContext.Provider value={{ user, setUser, week, setWeek }}>
+        <AppContext.Provider value={{ user: { name: '', id: '' }, setUser, week, setWeek }}>
             {children}
         </AppContext.Provider>
     )
