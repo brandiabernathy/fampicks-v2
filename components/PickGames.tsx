3cf3c13e49f60a37ac4f604a7c56ba5ddc5b4dc5
Brandi Abernathy <brandiabernathy22@gmail.com>
2023-10-18 20:55:23 -0700

    styling

--- components/PickGames.tsx
+++ components/PickGames.tsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import { db } from '../utils/firebase/config';
 import { getDoc, setDoc, doc, updateDoc } from "firebase/firestore";
 import { useAppContext } from '../context/app';
-import Game from './Game';
 import Image from 'next/image';
 import dayjs from 'dayjs';
 
@@ -36,23 +35,18 @@ export default function PickGames({ onClose }) {
 		fetch('http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?limit=1000&dates=20231016-20231023&groups=' + selectedConference)
 			.then((res) => res.json())
 			.then((data) => {
-                console.log("games data", data);
+                console.log("gamessss data", data);
 				setGames(data.events
 				.sort((a: any, b: any) => a.date < b.date ? -1 : 1)
 				.map((game: any) => ({
 					id: game.id,
 					date_long: game.date,
 					date: dayjs(game.date).format('M/D h:mmA'),
-					status: game.status,
 					home: game.competitions[0].competitors[0],
 					away: game.competitions[0].competitors[1],
-					home_rank: game.competitions[0].competitors[0].curatedRank,
-					away_rank: game.competitions[0].competitors[1].curatedRank,
-					broadcast: game.competitions[0].broadcasts.length ? game.competitions[0].broadcasts[0].names[0] : '',
-					winner: game.status.type.description == 'Final' ?
-											(game.competitions[0].competitors[0].winner ? game.competitions[0].competitors[0].id : game.competitions[0].competitors[1].id) : '',
 				})))
 			})
+            console.log("games", games);
 	}
 
     useEffect(() => {
@@ -62,10 +56,6 @@ export default function PickGames({ onClose }) {
         }
     }, [selectedConference])
 
-    let gamesList = games.map((game: any)=> {
-		return <Game key={game.id} game={game} />
-	});
-
 
 	return (
 		<div className="fixed top-0 right-0 bottom-0 left-0 z-20">
@@ -74,7 +64,7 @@ export default function PickGames({ onClose }) {
                 <div className="relative bg-white rounded p-8 drop-shadow">
                     <h2 className="text-2xl">Pick the games for Week {week}</h2>
                     <p>Which conference would you like to pick from?</p>
-                    { conferences.map((conference: any, index: any) => {
+                    { conferences.map((conference: any) => {
                         return (
                             <div key={conference.id}>
                                 <Image
@@ -88,7 +78,39 @@ export default function PickGames({ onClose }) {
                             </div>
                         )
                     })}
-                    <div className="grid gap-3 lg:grid-cols-3">{gamesList}</div>
+                    <div className="grid">
+                        { games.map((game: any, index: any) => {
+                            return (
+                                <div key={index} className="flex items-center">
+                                    <div className="flex flex-col items-center w-40">
+                                        <Image
+                                            src={game.away.team.logo}
+                                            alt={game.away.team.location}
+                                            width={80}
+                                            height={80}
+                                        />
+                                        <div className="text-center">
+                                            {game.away.curatedRank.current < 26 && <span className="text-slate-500 mr-1">{game.away.curatedRank.current}</span>}
+                                            {game.away.team.displayName}
+                                        </div>
+                                    </div>
+                                    <div className="mx-5">at</div>
+                                    <div className="flex flex-col items-center w-40">
+                                        <Image
+                                            src={game.home.team.logo}
+                                            alt={game.home.team.location}
+                                            width={80}
+                                            height={80}
+                                        />
+                                        <div className="text-center">
+                                            {game.home.curatedRank.current < 26 && <span className="text-slate-500 mr-1">{game.home.curatedRank.current}</span>}
+                                            {game.home.team.displayName}
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
                     { errorMsg && <div className="text-rose-600">{errorMsg}</div> }
 				</div>
 			</div>
